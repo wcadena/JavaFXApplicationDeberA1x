9d8a5544b328fa715ffc9717a8145024ab31ae0b
wcadena <wcadena@outlook.com>
2018-09-29 22:47:37 -0500

    Se aumenta primer controlador

--- src/javafxapplicationdebera1x/controlers/GeneradorController.java
+++ src/javafxapplicationdebera1x/controlers/GeneradorController.java
@@ -5,10 +5,36 @@
  */
 package javafxapplicationdebera1x.controlers;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import javafxapplicationdebera1x.model.Generador;
+
 /**
  *
  * @author wcade
  */
 public class GeneradorController {
+
+    public ArrayList<Generador>  lista_generadores;
+
+    public GeneradorController(ArrayList<Generador> lista_generadores) {
+        this.lista_generadores = lista_generadores;
+    }
+    public GeneradorController(Generador generador) {
+        ArrayList<Generador>  lista_generadores = new ArrayList<Generador>();
+        lista_generadores.add(generador);        
+        this.lista_generadores = lista_generadores;
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Generador> lista_generadores(){
+        return this.lista_generadores;
+    }
+    public void addGenerador(Generador generador){
+        this.lista_generadores.add(generador);
+    }
     
 }
