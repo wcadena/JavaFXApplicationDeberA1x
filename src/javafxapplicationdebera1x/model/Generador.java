ea1c92c48f0c8f1104b9572c79356f03ded6b85e
wcadena <wcadena@outlook.com>
2018-09-29 23:12:55 -0500

    Se aumenta opcion de creacion de modelo con el constructor y se da forma a la logica q se desea implmentar

--- src/javafxapplicationdebera1x/model/Generador.java
+++ src/javafxapplicationdebera1x/model/Generador.java
@@ -16,43 +16,98 @@ public class Generador {
     private String código;
     private intArray parametros;
 
+    /**
+     * Crea un generador, no usa id automatico por le momento.
+     * @param id
+     * @param fabricante
+     * @param modelo
+     * @param código
+     * @param parametros
+     */
+    public Generador(int id, String fabricante, String modelo, String código, intArray parametros) {
+        this.id = id;
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.código = código;
+        this.parametros = parametros;
+    }
+
+    /**
+     *
+     * @return
+     */
     public int getId() {
         return id;
     }
 
-
+    /**
+     *
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getFabricante() {
         return fabricante;
     }
 
+    /**
+     *
+     * @param fabricante
+     */
     public void setFabricante(String fabricante) {
         this.fabricante = fabricante;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getModelo() {
         return modelo;
     }
 
+    /**
+     *
+     * @param modelo
+     */
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCódigo() {
         return código;
     }
 
+    /**
+     *
+     * @param código
+     */
     public void setCódigo(String código) {
         this.código = código;
     }
 
+    /**
+     *
+     * @return
+     */
     public intArray getParametros() {
         return parametros;
     }
 
+    /**
+     *
+     * @param parametros
+     */
     public void setParametros(intArray parametros) {
         this.parametros = parametros;
     }
--- src/javafxapplicationdebera1x/model/ParametroGenerador.java
+++ src/javafxapplicationdebera1x/model/ParametroGenerador.java
@@ -12,6 +12,9 @@ import java.util.Date;
  * @author wcade
  */
 public class ParametroGenerador {
+    static final int PRINCIPAL  = 999999911; 
+    static final int DATO_SEC = 999999922; 
+    private int id;
     private String nombre;
     private int is_principal;//este valor indica si es el modelo de todo {1} osea este es el que indica como van los parametros y este va a ser el q tenga el ultimo valor , para todos los demas sera {0}
     private int rango_minimo;
@@ -22,74 +25,189 @@ public class ParametroGenerador {
     private Date update_at;
     private Date delete_at;
 
+    /**
+     * Aumenta datos de generador, usar este para principal o modelo
+     * 
+     * @param id
+     * @param nombre
+     * @param is_principal
+     * @param rango_minimo
+     * @param rango_maximo
+     * @param valor_actual
+     * @param tipo_alarma
+     */
+    public ParametroGenerador(int id, String nombre, int is_principal, int rango_minimo, int rango_maximo, int valor_actual, Alarma tipo_alarma) {
+        this.id = id;
+        this.nombre = nombre;
+        this.is_principal = is_principal;
+        this.rango_minimo = rango_minimo;
+        this.rango_maximo = rango_maximo;
+        this.valor_actual = valor_actual;
+        this.tipo_alarma = tipo_alarma;
+        this.created_at = new Date();
+    }
+    /**
+     * Aumenta datos de generador como registro
+     * 
+     * @param id
+     * @param nombre
+     * @param is_principal
+     * @param rango_minimo
+     * @param rango_maximo
+     * @param valor_actual
+     * @param tipo_alarma
+     */
+    public ParametroGenerador(int id, String nombre, int rango_minimo, int rango_maximo, int valor_actual, Alarma tipo_alarma) {
+        this.id = id;
+        this.nombre = nombre;
+        this.is_principal = ParametroGenerador.DATO_SEC;
+        this.rango_minimo = rango_minimo;
+        this.rango_maximo = rango_maximo;
+        this.valor_actual = valor_actual;
+        this.tipo_alarma = tipo_alarma;
+        this.created_at = new Date();
+    }
+
+    /**
+     *
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getIs_principal() {
         return is_principal;
     }
 
+    /**
+     *
+     * @param is_principal
+     */
     public void setIs_principal(int is_principal) {
         this.is_principal = is_principal;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getRango_minimo() {
         return rango_minimo;
     }
 
+    /**
+     *
+     * @param rango_minimo
+     */
     public void setRango_minimo(int rango_minimo) {
         this.rango_minimo = rango_minimo;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getRango_maximo() {
         return rango_maximo;
     }
 
+    /**
+     *
+     * @param rango_maximo
+     */
     public void setRango_maximo(int rango_maximo) {
         this.rango_maximo = rango_maximo;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getValor_actual() {
         return valor_actual;
     }
 
+    /**
+     *
+     * @param valor_actual
+     */
     public void setValor_actual(int valor_actual) {
         this.valor_actual = valor_actual;
     }
 
+    /**
+     *
+     * @return
+     */
     public Alarma getTipo_alarma() {
         return tipo_alarma;
     }
 
+    /**
+     *
+     * @param tipo_alarma
+     */
     public void setTipo_alarma(Alarma tipo_alarma) {
         this.tipo_alarma = tipo_alarma;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getCreated_at() {
         return created_at;
     }
 
+    /**
+     *
+     * @param created_at
+     */
     public void setCreated_at(Date created_at) {
         this.created_at = created_at;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getUpdate_at() {
         return update_at;
     }
 
+    /**
+     *
+     * @param update_at
+     */
     public void setUpdate_at(Date update_at) {
         this.update_at = update_at;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getDelete_at() {
         return delete_at;
     }
 
+    /**
+     *
+     * @param delete_at
+     */
     public void setDelete_at(Date delete_at) {
         this.delete_at = delete_at;
     }
